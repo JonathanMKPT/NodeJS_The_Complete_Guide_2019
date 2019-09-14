20b8769a122737fc0a5dccecbf592f5cdacb84d5
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-14 15:58:14 -0400

    Familiarized with the spread and rest operators

--- object.js
+++ object.js
@@ -6,4 +6,23 @@ const person = {
 	}
 };
 
-person.greet();
+const copiedPerson = {...person};
+console.log(copiedPerson);
+
+const hobbies = ["Coding","Drawing","Languages","Thinking"];
+//for(let hobby of hobbies){
+//		console.log(hobby);
+//}
+//console.log(hobbies.map(hobby => "Hobbies: " + hobby));
+//console.log(hobbies)
+//hobbies.push("Exercising")
+//console.log(hobbies)
+const copiedArray = [...hobbies]
+console.log(copiedArray);
+
+const toArray (...args) => {
+	return args;
+};
+
+console.log(toArray(1,2,3,4));
+
