6d078b8f1bf8313c511b98e37204c65d94cbfe14
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-14 16:06:41 -0400

    Used destruct functionality and commented out rest and spread functions, moving onto async next

--- object.js
+++ object.js
@@ -6,23 +6,34 @@ const person = {
 	}
 };
 
-const copiedPerson = {...person};
-console.log(copiedPerson);
+const printName = ({name}) => {
+	console.log(name);
+}
+
+printName(person);
+
+const {name, age} = person;
+console.log(name,age)
+
+//const copiedPerson = {...person};
+//console.log(copiedPerson);
 
 const hobbies = ["Coding","Drawing","Languages","Thinking"];
-//for(let hobby of hobbies){
-//		console.log(hobby);
-//}
-//console.log(hobbies.map(hobby => "Hobbies: " + hobby));
-//console.log(hobbies)
-//hobbies.push("Exercising")
-//console.log(hobbies)
-const copiedArray = [...hobbies]
-console.log(copiedArray);
+const [hobby1,hobby2] = hobbies;
+console.log(hobby1,hobby2);
+////for(let hobby of hobbies){
+////		console.log(hobby);
+////}
+////console.log(hobbies.map(hobby => "Hobbies: " + hobby));
+////console.log(hobbies)
+////hobbies.push("Exercising")
+////console.log(hobbies)
+//const copiedArray = [...hobbies]
+//console.log(copiedArray);
 
-const toArray (...args) => {
-	return args;
-};
+//const toArray (...args) => {
+	//return args;
+//};
 
-console.log(toArray(1,2,3,4));
+//console.log(toArray(1,2,3,4));
 
