d6905fb77bb75cb32c6d59afacbffdb13471023a
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-15 17:38:30 -0400

    logged url, method and headers in the console, used the res.write methodto add html message that works on the localhost....neat!

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-	console.log(req);
+	console.log(req.url,req.method,req.headers);
+	//process.exit();
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write('<head><title>My First Page in NodeJS</title></head>');
+	res.write('<body><h1>Hello Jonathan, you will be a software engineer!</h1></body>');
+	res.write('</html>');
+	res.end();
 });
 
 server.listen(3000);
