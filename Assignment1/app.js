33d1cba3d8820514e069d42e23e4977bef6f6e9a
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-23 18:29:53 -0400

    Added users route and tested locally, so far so good

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -6,8 +6,6 @@ const routes = require('./routes');
 const server = http.createServer(routes); 
 
 
-//3)Add a form with a "username" <input> to the "/" page and submit a
-//POST request to "/create-user" upon button click
 
 //4)Add the "/create-user" route and pars the incoming data(ie username)
 // and log it to the console.
--- Assignment1/routes.js
+++ Assignment1/routes.js
@@ -1,7 +1,7 @@
 //2) Handle 2 routes "/" and "/users"
-	//-Return some greeting text on "/"
-	//-Return a list of dummy users(<ul><li>User1</li></ul>)
-//requiring file system for rouets
+	
+	
+//requiring file system for routes
 const fs = require('fs');
 
 const requestHandler = (req, res) =>{
@@ -10,11 +10,25 @@ const requestHandler = (req, res) =>{
 	if(url === '/') {
 		res.write('<html>');
 		res.write('<head><title>Jonathan\'s Assignment 1 Project></title></head>');
+		//-Return some greeting text on "/"
 		res.write('<h1>Welcome to Jonathan\'s Assignment 1 Project for NodeJS</h1>');
-		res.write('<body><form action="/users"><input type="text" name="users"><button>Submit</button></form></body>');
+		//3)Add a form with a "username" <input> to the "/" page and submit a
+		//POST request to "/create-user" upon button click
+		res.write('<body><form action="/users" action="POST"><input type="username" name="users"><button>Submit User</button></form></body>');
 		res.write('</html>');
 		return res.end();
 	}
+
+	if(url === '/users'){
+		res.write('<html>');
+		res.write('<h1>Here are our default users</h1>');
+		//-Return a list of dummy users(<ul><li>User1</li></ul>)
+		res.write('<body><ul><li>Alpha</li><li>Bravo</li><li>Charlie</li></ul></body>')
+		res.write('</html>');
+		
+	}
+	
+
 };
 
 module.exports = requestHandler
