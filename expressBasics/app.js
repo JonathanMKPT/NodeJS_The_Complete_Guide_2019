a50c1d4fa549e9abbb5ebdc06cacdbcc4f6f5726
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-30 17:15:13 -0400

    Familiarized with express routing, moving onto assignment #2 next

--- expressBasics/app.js
+++ expressBasics/app.js
@@ -2,17 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res, next) => {
-	console.log("this is inside the middleware");
-	next(); //Allows request to go into the next middle ware
-
-
+app.use('/add-product',(req,res, next) => {
+	console.log("this is inside another middleware");
+	res.send('<h1>This is the "Add Product" page.., k bye!</h1>');	
 });
 
-app.use((req,res, next) => {
+app.use('/',(req,res, next) => {
 	console.log("this is inside another middleware");
-	res.send('<h1>Hello from express!</h1>');
-	
+	res.send('<h1>Hello from express!</h1>');	
 });
 
 app.listen(3000);
